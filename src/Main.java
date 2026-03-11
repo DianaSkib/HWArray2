32aa552d13c4cb36bb2557a602a5d7d78f05160b
Диана <ashkhar.ik@mail.ru>
2026-03-11 21:02:17 +0300

    task1

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,31 @@
+import java.util.Arrays;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        ///task1
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        int[] inputArray1 = {15630, 3000, 87236, 5486, 90000};
+        double[] outputArray1 = new double[4];
+        int sum = 0;
+        int max = inputArray1[0];
+        int min = inputArray1[0];
+        for (int payment : inputArray1) {
+            sum += payment;
+            if (payment > max) {
+                max = payment;
+            }
+            if (payment < min) {
+                min = payment;
+            }
         }
+        double average = (double) sum / inputArray1.length;
+        outputArray1[0] = sum;
+        outputArray1[1] = max;
+        outputArray1[2] = min;
+        outputArray1[3] = average;
+        System.out.println("inputArray1: " + Arrays.toString(inputArray1));
+        System.out.println("outputArray1: " + Arrays.toString(outputArray1));
     }
 }
